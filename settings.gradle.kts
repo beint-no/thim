828994be884118b1279920bd787ab9b34028ad01
greg taube <gregjotau@gmail.com>
2026-09-19 08:50:28 +0200

    Release 0.12.1 (#62)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ include("runtime", "compiler", "spring", "gradle-plugin", "settings-plugin", "ex
 
 gradle.lifecycle.beforeProject {
     group = "no.beint.thim"
-    version = "0.12.0"
+    version = "0.12.1"
     plugins.withId("java") {
         extensions.configure<JavaPluginExtension> {
             toolchain.languageVersion.set(JavaLanguageVersion.of(27))
